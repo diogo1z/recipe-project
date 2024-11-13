c716b6e09369ffee6faf3a0f07e04f3bc85e24ff
Diogo Matias <diogo1z@hotmail.com>
2024-11-13 13:24:19 -0300

    feat: adding resource message and culture middleware

--- src/back/Recipe.Application/UseCases/User/Register/RegisterUserValidation.cs
+++ src/back/Recipe.Application/UseCases/User/Register/RegisterUserValidation.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using Recipe.Communication.Requests;
+using Recipe.Exceptions;
 
 namespace Recipe.Application.UseCases.User.Register;
 
@@ -7,20 +8,14 @@ public class RegisterUserValidation : AbstractValidator<RequestRegisterUserJson>
 {
     public RegisterUserValidation()
     {
-        RuleFor(user => user.Email)
-            .NotEmpty();
-
-        RuleFor(user => user.Email)
-            .EmailAddress();
-
-        RuleFor(user => user.Name)
-        .NotEmpty()
-        .MinimumLength(3);
-
+        RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
+        RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.EMAIL_EMPTY);
         RuleFor(user => user.Password)
             .NotEmpty()
             .MinimumLength(6);
-
-
+        When(user => !string.IsNullOrEmpty(user.Email), () =>
+        {
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+        });
     }
 }
